0274c0def96a0ecc7484fe5dc2c57ea46efd6402
theridwanade <theridwanade@gmail.com>
2025-11-22 00:04:14 +0100

    feat(main): add health check endpoint to server
    - Implemented a new route "/health" that responds with "OK"
    - This allows for basic health monitoring of the server

--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -1,10 +1,15 @@
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }) );
-    
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(health_check));
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Server running on http://localhost:3000");
     axum::serve(listener, app).await.unwrap();
+}
+async fn health_check() -> &'static str {
+    "OK"
 }
