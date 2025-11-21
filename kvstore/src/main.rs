d87ee3173559ce9eef69a120ff1ea2ce3d1a96c0
theridwanade <theridwanade@gmail.com>
2025-11-21 23:56:55 +0100

    feat(main): implement basic server with axum and tokio
    - Added axum and tokio dependencies to Cargo.toml
    - Set up a simple HTTP server that responds with "Hello, World!" on the root route
    - Configured the server to listen on port 3000

--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }) );
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server running on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
 }
